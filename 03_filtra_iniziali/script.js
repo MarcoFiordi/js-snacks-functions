7280e67793710aa4d4b228671728149ec356d45c
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-01 17:16:39 +0200

    finalizzazione terzo esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtraPerLettera(arrayNomi, lettera) {
+    const risultato = []
+    for (let i = 0; i < arrayNomi.length; i++) {
+        const nome = arrayNomi[i];
+        const primaLettera = nome[0];
+        if (primaLettera === lettera) {
+            risultato.push(nome);
+        }
+        //console.log(nome, primaLettera, lettera);
+        
+    }
+    return risultato;
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(filtraPerLettera(names,'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
