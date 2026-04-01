c1e4f380dac9fa37b4bf96f2aa481ccdaf446333
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-01 16:36:39 +0200

    finalizzazione secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -3,11 +3,21 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
-// Dichiara la funzione qui.
 
+// Dichiara la funzione qui.
+function getIniziali(arrayNomi) {
+    const iniziali = [];
+    for (let i = 0; i < arrayNomi.length; i++){
+        const nome = arrayNomi[i];
+        const primaLettera = nome[0];
+        iniziali.push(primaLettera);
+    }
+    return iniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getIniziali(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
